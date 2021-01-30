5bc84e257afecfb8390f0c469c425e0b1ac0b099
Bruno Fernando Feitoza <bfeitoza634cyt@gmail.com>
2021-01-30 11:40:37 -0300

    feat: loading with lottie

--- src/components/LoadingWidget/index.tsx
+++ src/components/LoadingWidget/index.tsx
@@ -1,12 +1,33 @@
+import Lottie from 'react-lottie';
+
+import animationData from './loading.json';
+
 import Widget from '../Widget';
 
 export default function LoadingWidget() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+
   return (
     <>
       <Widget>
         <Widget.Header>Carregando...</Widget.Header>
 
-        <Widget.Content>[Desafio do Loading]</Widget.Content>
+        <Widget.Content>
+          <Lottie
+            options={defaultOptions}
+            height={100}
+            width={100}
+            isStopped={false}
+            isPaused={false}
+          />
+        </Widget.Content>
       </Widget>
     </>
   );
